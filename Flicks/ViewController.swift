2fd846c5a858a1cd27bbfd46b4d7bdc7322beafc
Syed Hakeem Abbas <hakeemsyd@fb.com>
2017-09-10 22:53:32 -0700

    first commit

---
+++ Flicks/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Flicks
+//
+//  Created by Syed Hakeem Abbas on 9/10/17.
+//  Copyright © 2017 codepath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
