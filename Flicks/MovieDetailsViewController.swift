e8d9507f668c18d74f08a5450fb85d4d744813e8
Syed Hakeem Abbas <hakeemsyd@fb.com>
2017-09-12 03:13:45 -0700

    Add details controller and nav

---
+++ Flicks/MovieDetailsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MovieDetailsViewController.swift
+//  Flicks
+//
+//  Created by Syed Hakeem Abbas on 9/12/17.
+//  Copyright © 2017 codepath. All rights reserved.
+//
+
+import UIKit
+
+class MovieDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
