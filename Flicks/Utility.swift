837cd257042838969809709e7ecacc9a6def3aac
Syed Hakeem Abbas <hakeemsyd@fb.com>
2017-09-14 16:24:30 -0700

    fix nil ref when path of image is null

--- Flicks/Utility.swift
+++ Flicks/Utility.swift
@@ -20,28 +20,29 @@ struct Utility {
     static let KEY_DEFAULT_VIEW_MODE = "default_view_mode";
     
     static func loadPhoto(withUrl url: String, into view: UIImageView){
-        let imageRequest = URLRequest(url: URL(string: url)!)
-        
-        view.setImageWith(
-            imageRequest,
-            placeholderImage: nil,
-            success: { (imageRequest, imageResponse, image) -> Void in
-                
-                // imageResponse will be nil if the image is cached
-                if imageResponse != nil {
-                    print("Image was NOT cached, fade in image")
-                    view.alpha = 0.0
-                    view.image = image
-                    UIImageView.animate(withDuration: 0.3,animations: { () -> Void in
-                        view.alpha = 1.0
-                    })
-                } else {
-                    print("Image was cached so just update the image")
-                    view.image = image
-                }
-        },
-            failure: { (imageRequest, imageResponse, error) -> Void in
-                // do something for the failure condition
-        })
+        if let u = URL(string: url) {
+            let imageRequest = URLRequest(url: u)
+            view.setImageWith(
+                imageRequest,
+                placeholderImage: nil,
+                success: { (imageRequest, imageResponse, image) -> Void in
+                    
+                    // imageResponse will be nil if the image is cached
+                    if imageResponse != nil {
+                        print("Image was NOT cached, fade in image")
+                        view.alpha = 0.0
+                        view.image = image
+                        UIImageView.animate(withDuration: 0.3,animations: { () -> Void in
+                            view.alpha = 1.0
+                        })
+                    } else {
+                        print("Image was cached so just update the image")
+                        view.image = image
+                    }
+            },
+                failure: { (imageRequest, imageResponse, error) -> Void in
+                    // do something for the failure condition
+            })
+        }
     }
 }
